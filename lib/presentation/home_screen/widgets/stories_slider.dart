a1701876bd52e798b12f1a682ade0ed7deda9c4d
najeebnaju512 <najeebnaju512@gmail.com>
2024-04-25 11:09:24 +0530

    story slider

--- lib/presentation/home_screen/widgets/stories_slider.dart
+++ lib/presentation/home_screen/widgets/stories_slider.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:socialmedia/core/constants/colors.dart';
 
 import '../../../core/constants/text_styles.dart';
@@ -14,68 +16,81 @@ class _StorySliderState extends State<StorySlider> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          "Feed",
-          style: GLTextStyles.ralewayStyl(
-              size: 25, weight: FontWeight.w500, color: ColorTheme.color4),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-            child: Padding(
-                padding: const EdgeInsets.only(left: 15),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: ColorTheme.grey,
+    return SizedBox(
+      height: size.width*.45,
+      child: Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Feed",
+              style: GLTextStyles.ralewayStyl(
+                  size: 25, weight: FontWeight.w600, color: ColorTheme.color4),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: ColorTheme.grey,
+                        ),
+                        height: size.width * .31,
+                        width: size.width * .15,
                       ),
-                      height: size.height * .149,
-                      width: size.width * .13,
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                          shrinkWrap: true,
-                          // physics: FixedExtentScrollPhysics(),
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 10,
-                          itemBuilder: (context, index) {
-                            return Row(
-                              children: [
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                Stack(children: [
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: Colors.red,
-                                    ),
-                                    height: size.height * .149,
-                                    width: size.width * .29,
+                      Positioned(
+                                  left: 5,
+                                  top: 12,
+                                  child: Icon(Icons.add_a_photo,size: size.width * .07,)
+                                )
+                    ],
+                  ),
+                  SizedBox(width: size.width*.01,),
+                  Expanded(
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        // physics: FixedExtentScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return Row(
+                            children: [
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Stack(children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.red,
                                   ),
-                                  Positioned(
-                                    left: 12,
-                                    top: 8,
-                                    child: CircleAvatar(
-                                      backgroundColor: Colors.white,
-                                      radius: 17,
-                                    ),
-                                  )
-                                ]),
-                              ],
-                            );
-                          }),
-                    )
-                  ],
-                ))),
-      ],
+                                  height: size.width * .31,
+                                  width: size.width * .29,
+                                ),
+                                Positioned(
+                                  left: 12,
+                                  top: 12,
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    radius: size.width * .04,
+                                  ),
+                                )
+                              ]),
+                            ],
+                          );
+                        }),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

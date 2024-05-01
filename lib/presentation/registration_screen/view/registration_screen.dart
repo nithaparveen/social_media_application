482a2ede18e46021fe6bfcf7370e8c2e4a2327ff
NITHA <nithaparveenk@gmail.com>
2024-05-01 15:54:50 +0530

    regscreen modified

--- lib/presentation/registration_screen/view/registration_screen.dart
+++ lib/presentation/registration_screen/view/registration_screen.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:socialmedia/core/constants/colors.dart';
 import 'package:socialmedia/core/constants/text_styles.dart';
-import 'package:socialmedia/global_widgets/text_form_field.dart';
-
+import 'package:socialmedia/presentation/login_screen/view/login_screen.dart';
 import '../../../global_widgets/title_and_textformfield.dart';
 
 class RegistrationScreen extends StatelessWidget {
@@ -14,50 +13,81 @@ class RegistrationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.sizeOf(context);
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: size.width,
-            decoration: BoxDecoration(
-                color: ColorTheme.blue,
-                borderRadius: BorderRadius.only(
-                    bottomRight: Radius.elliptical(350, 170),
-                    bottomLeft: Radius.elliptical(150, 20))),
-            height: size.height * .38,
-            // width: size.width * .85,
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              "Create \naccount",
-              textAlign: TextAlign.start,
-              style: GLTextStyles.poppinsStyl(
-                  color: ColorTheme.yellow,
-                  size: 28,
-                  weight: FontWeight.bold),
-            ),
-          ),
-          SingleChildScrollView(padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                Text(
-                  "Please SignUp to continue",
-                  textAlign: TextAlign.start,
-                  style: GLTextStyles.poppinsStyl(
-                    color: ColorTheme.yellow,
-                    size: 15,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: size.width,
+              decoration: BoxDecoration(
+                  color: ColorTheme.blue,
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.elliptical(350, 140),
+                      bottomLeft: Radius.elliptical(150, 40))),
+              height: size.height * .28,
+              // width: size.width * .85,
+              alignment: Alignment.bottomLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 45, top: 55),
+                    child: Text(
+                      "Create \naccount",
+                      textAlign: TextAlign.start,
+                      style: GLTextStyles.poppinsStyl(
+                          color: ColorTheme.yellow,
+                          size: 30,
+                          weight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: size.height * .2,
-                ),
-                TitleAndTextFormField(
-                  text: '',
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 48),
+                    child: Text(
+                      "Please SignUp to continue",
+                      textAlign: TextAlign.start,
+                      style: GLTextStyles.poppinsStyl(
+                        color: ColorTheme.yellow,
+                        size: 13,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 15,right: 15,top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TitleAndTextFormField(
+                    text: 'Name',
+                  ),
+                  TitleAndTextFormField(
+                    text: 'Username',
+                  ),
+                  TitleAndTextFormField(
+                    text: 'Email',
+                  ),TitleAndTextFormField(
+                    text: 'Password',
+                  ),
+                  SizedBox(height: size.height*.022,),
+                  MaterialButton(
+                      color: ColorTheme.blue,
+                      minWidth: size.width * .5,
+                      height: size.height * .07,
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen(),));
+                      },
+                      child: Text(
+                        "SignUp",
+                        style: GLTextStyles.leagueSpartan(
+                            size: 18, color: ColorTheme.yellow),
+                      )),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

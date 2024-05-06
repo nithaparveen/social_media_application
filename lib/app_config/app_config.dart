3d3cb30a459195924231a843f0a3a7d0ef26323c
NITHA <nithaparveenk@gmail.com>
2024-05-06 14:45:33 +0530

    app config modified

--- lib/app_config/app_config.dart
+++ lib/app_config/app_config.dart
@@ -1,9 +1,9 @@
 class AppConfig {
   //baseurl is set here
-  static String baseurl = "http://10.11.0.90:8000/";
+  static String baseurl = "http://10.11.0.72:8000/";
 
   // //key to store data from api function in LoginController in json format used in AppUtils to get access key & userid
-  // static const String loginData = 'logInData';
+  static const String loginData = 'logInData';
   //
   // //key to store data from api function in LoginController in json format used in AppUtils to get user details only
   // static const String userData = 'userdata';
@@ -14,5 +14,7 @@ class AppConfig {
   // //key to check whether the user is already registered  or not and navigate to login page on every other startup since registering.
   // static const String status = "status";
   //
-  // static const String token = "token";
+  static const String token = "token";
+  static const String loggedIn = 'loggedIn';
+
 }
